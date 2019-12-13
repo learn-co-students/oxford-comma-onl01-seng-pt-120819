7fcb0722e098f89c7978d6f80cf93d5560cd960c
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-13 14:52:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    p array.join
+  
+  elsif array.length==2
+   p array.join(" and ")
+  
+ elsif array.length >= 3
+   adding_last_and = "and #{array[-1]}"
+   array.pop
+   array.push(adding_last_and)
+   p array.join(", ")
+    end
 end
+
+
