02fa41f7af13972881a64e07a48f9419b87cf430
kowalix007 <kowalix007@gmail.com>
2019-12-17 17:24:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
 
+if array.length == 1 
+   array.join
+
+elsif
+  array.length == 2
+  array = array.join(" and ")
+
+elsif 
+  array.length == 3
+  array = array.insert( -2, "and" ).join(", ")
+  array.sub! "and," , "and"
+  array
+else 
+  array = array.insert( -2, "and" ).join(", ")
+  array.sub! "and," , "and"
+  array 
+ end
 end
+
