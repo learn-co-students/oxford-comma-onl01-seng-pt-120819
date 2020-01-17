b845788e96db6ebe1a25e6d7daa895b9bea5e5dc
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-01-17 22:16:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  array.join(", ")
 end
+
+def oxford_comma(array)
+  array.join(" and ")
+end
+  
+def oxford_comma(array)
+  array.size <= 2? array.join(" and ") : (array.first array.size - 1).join(", ") + ", and #{array.last}"
+end
+
